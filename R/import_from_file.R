4f61865a1c91cae270b9cfcd5fe88336902a116c
Stevan Earl <stevan.earl@asu.edu>
2019-07-23 16:06:23 -0700

    add spatial location of gauges R; reach length to kilometers

--- R/import_from_file.R
+++ R/import_from_file.R
@@ -1,7 +1,7 @@
 
 # README ------------------------------------------------------------------
 
-# Centralized location for importing data from files.
+# Centralized location for importing existing data stored in files.
 
 
 # libraries ---------------------------------------------------------------
@@ -22,6 +22,13 @@ options(scipen = 999)
 # note specifying the time zone on import, which is critical to maintaining the
 # integrity of these data as readr will otherwise convert dates/times to UTC
 # without warning nor shifting the corresponding data accordingly
+
+# total N and P:
+# per Marisa, "AQ2 and TrAAcs measure total phosphorous as phosphate, but
+# samples for Lachat are filtered so it's measuring dissolved phosphorus as
+# phosphate. So I think the TrAAcs data can be combined with its replacement-
+# the AQ2." thus NO3T_AQ2 and NO3T_TRAACS" are comingled to "NO3T", and
+# "PO4T_AQ2 and PO4T_TRAACS" are comingled to "PO4T".
 
 ibwQchem <- read_csv('https://www.dropbox.com/s/wsseakmze4hsnws/ibwQchem.csv?dl=1',
                      locale = locale(tz = "America/Phoenix"),  # note !!!
@@ -66,8 +73,21 @@ contributingGauges <- read_csv('https://www.dropbox.com/s/7mgz8ajt0f8788i/contri
 
 contributingGauges[is.na(contributingGauges)] <- 0 # NAs to zero
 
-# factor
+# add reach lengths (factor (reachLength) and continuous (reachLengthKilo)) and
+# input from channels to the main stem. See ibw_gauges_spatial.R for details
+# regarding reachLengthKilo
 contributingGauges <- contributingGauges %>% 
+  mutate(
+    reachLengthKilo = case_when(
+      reachLength == 0 ~ 0,
+      reachLength == 1 ~ 1,
+      reachLength == 2 ~ 6.25,
+      reachLength == 3 ~ 9.5,
+      reachLength == 4 ~ 10.8,
+      reachLength == 5 ~ 18.3,
+      reachLength == 6 ~ 22.65
+    )
+  ) %>% 
   mutate(
     reachLength = as.factor(reachLength),
     fromGraniteReef = as.factor(fromGraniteReef),
