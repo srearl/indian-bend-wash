50c1734e73d60891923c7bcf4e86f340fcc846a1
Stevan <stevan.earl@asu.edu>
2019-08-01 13:59:53 -0700

    update spatial data to include Curry

--- R/ibw_gauges_spatial.R
+++ R/ibw_gauges_spatial.R
@@ -32,15 +32,26 @@ ibwGuages <- c(4603,
                4693,
                4728)
 
-MCFCD_station_metadata <- read_csv('ALERT_sensors_all_by_ID.csv')
+# read MCFCD station metadata in file: `ALERT_sensors_all_by_ID.csv`
+MCFCD_station_metadata <- read_csv('https://www.dropbox.com/s/j2ep0idsroir0tn/ALERT_sensors_all_by_ID.csv?dl=1')
 
 MCFCD_station_metadata %>% 
   filter(DEV_ID %in% ibwGuages) %>% 
   separate(STA_LAT_DMS, c("lat_d", "lat_m", "lat_s"), sep = " ") %>% 
   separate(STA_LONG_DMS, c("long_d", "long_m", "long_s"), sep = " ") %>% 
+  # manually add the USGS station location
+  add_row(
+    STA_NAME = "IBW @ Curry Rd.",
+    lat_d = "33",
+    lat_m = "26",
+    lat_s = "25.75",
+    long_d = "111",
+    long_m = "54",
+    long_s = "55.5",
+  ) %>% 
   mutate(
     lat = as.numeric(lat_d) + (as.numeric(lat_m)/60) + (as.numeric(lat_s)/3600),
     lon = (as.numeric(long_d) + (as.numeric(long_m)/60) + (as.numeric(long_s)/3600))*-1
-  ) %>% 
+  ) %>%
   st_as_sf(coords = c("lon", "lat"), crs = 4326) %>% 
-  st_write("ibwGauges.kml", driver='kml')
+  st_write("~/Desktop/ibwGauges.kml", driver='kml')
