bd1f9aeed00ddcc42b50f0eb15e8c643d316c01e
JCI <jamie.irby@email.ucr.edu>
2026-04-27 13:54:52 -0700

    precipitation metrics
    Cumulative ppt in rolling windows of various durations before each storm sample (week, 1 month, 3 months), storm size, storm intensity(cumulative/duration of storm)

--- R/precipitation.R
+++ R/precipitation.R
@@ -14,29 +14,107 @@ library(googledrive)
 library(googlesheets4)
 library(lubridate)
 library(rio)
+library(zoo)
 
 
-hourly_cols <- c(
-  "year", "doy", "hour",
-  "air_temp", "rel_humidity", "vpd", "solar_rad", "precip",
-  "soil_temp_4in", "soil_temp_20in",
-  "wind_speed_avg", "wind_vec_mag", "wind_vec_dir", "wind_dir_sd", "wind_speed_max",
-  "eto",
-  "actual_vapor_pressure", "dewpoint"
-)
+## Uncomment lines below to download raw data for the first time
+# hourly_cols <- c(
+#   "year", "doy", "hour",
+#   "air_temp", "rel_humidity", "vpd", "solar_rad", "precip",
+#   "soil_temp_4in", "soil_temp_20in",
+#   "wind_speed_avg", "wind_vec_mag", "wind_vec_dir", "wind_dir_sd", "wind_speed_max",
+#   "eto",
+#   "actual_vapor_pressure", "dewpoint"
+# )
+# 
+# UCR_new <- drive_get(as_id("https://drive.google.com/drive/folders/1wP4iKeIlEfKU_tWOV8zc8Lau8cqAlNrq"))
+# UCR_glist <- drive_ls(UCR_new, type = "txt")
+# 
+# setwd(here("Data/precip"))
+# walk(UCR_glist$id, ~ drive_download(as_id(.x), overwrite = TRUE))
+# 
+# file_list <- list.files(recursive = FALSE, pattern = "\\.txt$", full.names = TRUE)
+# 
+# ppt_raw <- map_dfr(
+#   file_list,
+#   ~ read.csv(.x, header = FALSE, col.names = hourly_cols)
+# )
+# 
+# setwd(here())
+# 
+# write.csv(ppt_raw, here("Data/precip", "ppt_raw.csv"))
 
-UCR_new <- drive_get(as_id("https://drive.google.com/drive/folders/1wP4iKeIlEfKU_tWOV8zc8Lau8cqAlNrq"))
-UCR_glist <- drive_ls(UCR_new, type = "txt")
+ppt_raw <- read_csv(here("Data/precip", "ppt_raw.csv")) %>% select(-`...1`)
 
-setwd(here("Data/precip"))
-walk(UCR_glist$id, ~ drive_download(as_id(.x), overwrite = TRUE))
+#### CALCULATE METRICS ####
+## cumulative ppt in rolling windows of various durations before each storm sample (1, 3 months, week)
 
-file_list <- list.files(recursive = FALSE, pattern = "\\.txt$", full.names = TRUE)
+ppt_all <- ppt_raw %>% select(c(year, doy, hour, precip)) %>% 
+  group_by(year, doy) %>%
+  mutate(daily_tot = sum(precip)) 
 
-ppt_raw <- map_dfr(
-  file_list,
-  ~ read.csv(.x, header = FALSE, col.names = hourly_cols)
-)
 
-setwd(here())
+ppt_roll <- ppt_all %>%
+  group_by(year) %>%
+  mutate(ppt_30day_cum = rollapply(daily_tot, width = 30, align = "right", FUN = sum, fill = NA, na.rm = TRUE),
+         ppt_30day_avg = rollapply(daily_tot, width = 30, align = "right", FUN = mean, fill = NA, na.rm = TRUE),
+         ppt_10day_cum = rollapply(daily_tot, width = 10, align = "right", FUN = sum, fill = NA, na.rm = TRUE),
+         ppt_10day_avg = rollapply(daily_tot, width = 10, align = "right", FUN = mean, fill = NA, na.rm = TRUE),
+         ppt_5day_cum = rollapply(daily_tot, width = 5, align = "right", FUN = sum, fill = NA, na.rm = TRUE),
+         ppt_5day_avg = rollapply(daily_tot, width = 5, align = "right", FUN = mean, fill = NA, na.rm = TRUE)) %>%
+  ungroup()
+
+## calculate storm size, storm intensity
+# storm delination - original code from q_storms.R
+
+assign_storm <- function(df, threshold = 0, dry_steps = 24){
+  
+  #df <- df %>% arrange(datetime)
+  
+  dry <- df$precip <= threshold
+  
+  # count consecutive dry steps
+  dry_count <- ave(dry, cumsum(!dry), FUN = seq_along)
+  dry_count[!dry] <- 0
+  
+  # storm starts when flow rises above threshold after sufficient dryness
+  storm_start <- df$precip > threshold &
+    dplyr::lag(dry_count, default = 0) >= dry_steps
+  
+  # handle dataset starting mid-storm
+  storm_start[1] <- df$precip[1] > threshold
+  
+  df %>%
+    mutate(
+      storm_start = storm_start,
+      storm_id = cumsum(storm_start)
+    )
+}
+
+ppt_storms <- assign_storm(ppt_roll)
+
+# storm size - total precip per storm, storm intensity = cumulative/duration of storm
+ppt_storms <- ppt_storms %>% group_by(storm_id) %>% mutate(storm_size = sum(precip), 
+                                                           storm_duration_hr = row_number(),
+                                                           storm_intensity = storm_size / max(storm_duration_hr), 
+                                                           Date = as.Date(doy, origin = "2002-12-31"), 
+                                                           datetime = as.POSIXct(
+                                                             paste(year, doy, hour),
+                                                             format = "%Y %j %H",
+                                                             tz = "UTC")
+                                                           )
+
+write.csv(ppt_storms, here("Data/precip", "ibw_precip.csv"))
+
+#### PLOTTING ####
+
+ppt_storms %>% ggplot(aes(x = datetime, y = precip))+
+  geom_col() +
+  labs(title = "Hourly Precip") +
+  facet_wrap(~year, scales = "free") + theme_classic()
+
+ppt_storms %>% ggplot(aes(x = Date, y = daily_tot))+
+  geom_col() +
+  labs(title = "Daily Precip", y = "precip (mm)") +
+  facet_wrap(~year, scales = "free") + theme_classic()
 
